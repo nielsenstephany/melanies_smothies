0c5929722ec4f01c63910164dee01ab67b18ea14
Stephany Nielsen <40550043+nielsenstephany@users.noreply.github.com>
2026-04-18 12:29:19 -0700

    Update streamlit_app.py
    added name on order part

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,10 @@ st.write(
 cnx = st.connection("snowflake")
 session = cnx.session()
 
+#name on order
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be:", name_on_order)
+
 #creating the dataframe
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)

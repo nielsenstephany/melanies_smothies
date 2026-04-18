51dad771deee4be600aeb09e62582973d356eb1f
Stephany Nielsen <40550043+nielsenstephany@users.noreply.github.com>
2026-04-18 13:05:00 -0700

    Update streamlit_app.py
    SmoothieFroot Data to Show Nutrition Data for the Fruits Chosen

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,12 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        
+# Display Smoothiefroot nutrition information from API
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-  # Build INSERT statement
+# Build INSERT statement
     my_insert_stmt = """ insert into smoothies.public.orders
                 (INGREDIENTS, NAME_ON_ORDER)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
@@ -48,8 +52,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-
-# Display Smoothiefroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)

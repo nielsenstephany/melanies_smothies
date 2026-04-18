0d75ba655b047696969a4f9107a7d42981a94a22
Stephany Nielsen <40550043+nielsenstephany@users.noreply.github.com>
2026-04-18 12:35:18 -0700

    Update streamlit_app.py
    adding request library to connect to REST API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages.
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app.
@@ -47,3 +48,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
+ 
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)

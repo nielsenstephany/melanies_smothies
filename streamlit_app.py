0e6bbf9040bd236ab04c566dbfd7af9a84c73c28
Stephany Nielsen <40550043+nielsenstephany@users.noreply.github.com>
2026-04-18 10:56:42 -0700

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages.
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col, when_matched
 
 # Write directly to the app.
@@ -10,7 +9,11 @@ st.write(
   """
 )
 
-session = get_active_session()
+#session config
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+#creating the dataframe
 my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED") == 0).collect()
 
 if my_dataframe:

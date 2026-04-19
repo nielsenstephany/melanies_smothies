6c3f7e4abe4e01619bbc9b8b35909b44b4c2d789
Stephany Nielsen <40550043+nielsenstephany@users.noreply.github.com>
2026-04-19 11:48:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ if ingredients_list:
         
 # Display Smoothiefroot nutrition information from API
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/{search_on})
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 # Build INSERT statement
